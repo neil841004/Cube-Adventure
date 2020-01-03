7da28cc378c2f6d31f30c9a38d1f38cd2e570dd7
Halaa <neil841004@gmail.com>
2020-01-03 20:03:40 +0800

    紀錄點

---
+++ Assets/Scripts/BetterJumping.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetterJumping : MonoBehaviour
+{
+    Rigidbody rb;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,54 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Rewired;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Player player;
     Rigidbody rb;
     public float margin;
     public float speed = 5f;
     public float jumpForce = 500f;
     public bool isJump = false;
-    public float fallMultiplier = 2.5f;
-    public float lowJumpMultiplier = 2f;
-    public bool canMove= true;
+    public bool canMove = true;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        player = ReInput.players.GetPlayer(0);
     }
     private void Update()
     {
-        if(player.GetButtonDown("Jump")){
-            if(IsGrounded())Jump();
-            if(IsPushWall()&&!IsGrounded())WallJump();
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (IsGrounded()) Jump();
+            if (IsPushWall() && !IsGrounded()) WallJump();
         }
-        if(IsPushWall()){
+        if (IsPushWall())
+        {
             rb.velocity = new Vector2(rb.velocity.x, -0.5f);
         }
+        Debug.Log(rb.velocity.y);
     }
     private void FixedUpdate()
     {
-        if(!IsPushWall()&&canMove){
-        Move();
+        if (!IsPushWall() && canMove)
+        {
+            Move();
         }
     }
     void Move()
     {
-        float moveHorizontal = player.GetAxis("Move Horizontal");
-        float movement = player.GetAxis("Move Horizontal") * speed;
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float movement = Input.GetAxis("Horizontal") * speed;
         rb.velocity = new Vector2(movement, rb.velocity.y);
     }
     void Jump()
     {
-            rb.AddForce(Vector3.up * jumpForce);
-        if(rb.velocity.y<0){
-            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
-        } else if (rb.velocity.y>0&&!player.GetButton("Jump")){
-            rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
-        }
+        rb.AddForce(Vector3.up * jumpForce);
     }
     bool IsGrounded()
     {
@@ -61,24 +55,34 @@ public class PlayerMovement : MonoBehaviour
         else
             return false;
     }
-    int IsWallSide(){
-        Vector3 v3a = new Vector3(transform.position.x, transform.position.y - 0.25f, transform.position.z);
-        Vector3 v3b = new Vector3(transform.position.x, transform.position.y + 0.25f, transform.position.z);
-        if (Physics.Raycast(v3a, Vector3.right, margin)||Physics.Raycast(v3b, Vector3.right, margin)){
+    int IsWallSide()
+    {
+        Vector3 v3a = new Vector3(transform.position.x, transform.position.y - 0.24f, transform.position.z);
+        Vector3 v3b = new Vector3(transform.position.x, transform.position.y + 0.24f, transform.position.z);
+        if (Physics.Raycast(v3a, Vector3.right, margin) || Physics.Raycast(v3b, Vector3.right, margin))
+        {
             return 1; //右邊是牆壁
-        }else if (Physics.Raycast(v3a, -Vector3.right, margin)||Physics.Raycast(v3b, -Vector3.right, margin)){
+        }
+        else if (Physics.Raycast(v3a, -Vector3.right, margin) || Physics.Raycast(v3b, -Vector3.right, margin))
+        {
             return -1; //左邊是牆壁
         }
         else return 0;
     }
-    bool IsPushWall(){
-        if(IsWallSide()==1&&player.GetAxis("Move Horizontal")>0){
+    bool IsPushWall()
+    {
+        if (IsWallSide() == 1 && Input.GetAxis("Horizontal") > 0)
+        {
             return true;
-        }else if(IsWallSide()==-1&&player.GetAxis("Move Horizontal")<0){
+        }
+        else if (IsWallSide() == -1 && Input.GetAxis("Horizontal") < 0)
+        {
             return true;
-        }else return false;
+        }
+        else return false;
     }
-    void WallJump(){
+    void WallJump()
+    {
         StopCoroutine(DisableMovement(0));
         StartCoroutine(DisableMovement(.12f));
         rb.AddForce(Vector3.up * jumpForce);
