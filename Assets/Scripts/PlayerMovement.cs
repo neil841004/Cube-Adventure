a0404a402961037354928c6a23f9ef8eec9758d7
Halaa <neil841004@gmail.com>
2020-01-06 22:30:25 +0800

    修正FixedUpdate呼叫問題

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,12 +12,14 @@ public class PlayerMovement : MonoBehaviour
     public float margin;
     public float speed = 5f;
     public float jumpForce = 500f;
+    public float fallSpeedMax = -15f;
 
     [Space]
     [Header("Booleans")]
+    public bool startJump = false;
+    public bool isWallJump = false;
     public bool isJumpUp = false;
     public bool canMove = true;
-    public bool isWallJump = false;
 
     void Start()
     {
@@ -27,37 +29,51 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        
-        if(!IsPushWall()){
-            GetComponent<BetterJumping>().fallMultiplier = 2.5f;
+        if (Input.GetButtonDown("Jump")) // 跳躍狀態判斷
+        {
+            if ((!IsPushWall() && coll.OnGround()) || (IsPushWall() && coll.OnGround())) startJump = true;
+            else if (IsPushWall() && !coll.OnGround()) isWallJump = true;
         }
         if (rb.velocity.y < 0)
         {
             isJumpUp = false;
         }
-        if(Input.GetKeyDown(KeyCode.R)){
-            transform.position = new Vector3(0,2.75f,-0.5f);
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.position = new Vector3(0, 2.75f, -0.5f);
         }
     }
     private void FixedUpdate()
     {
         Move();
-        if (Input.GetButtonDown("Jump")) // 跳躍狀態判斷
-        {
-            if ((!IsPushWall() && coll.OnGround()) || (IsPushWall() && coll.OnGround())) Jump();
-            else if (IsPushWall() && !coll.OnGround()) WallJump();
-        }
-        if (IsPushWall() && !isWallJump)
-        {
-            rb.velocity = new Vector2(0, rb.velocity.y);
-            GetComponent<BetterJumping>().fallMultiplier = 0.2f;
-        }
+        Jump();
+        if (IsPushWall() && !coll.OnGround()) WallJump();
         if (isJumpUp)
         {
             if (Input.GetButtonUp("Jump"))
             {
                 rb.velocity = new Vector3(rb.velocity.x, 0);
             }
+        }
+        if (!isJumpUp)
+        {
+            if (rb.velocity.y <= fallSpeedMax)
+            {
+                rb.velocity = new Vector3(rb.velocity.x, fallSpeedMax);
+            }
+        }
+        if (IsPushWall() && !isWallJump)
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+            GetComponent<BetterJumping>().fallMultiplier = 0.2f;
+            if (rb.velocity.y <= fallSpeedMax * 0.5f)
+            {
+                rb.velocity = new Vector3(rb.velocity.x, fallSpeedMax * 0.5f);
+            }
+        }
+        if (!IsPushWall())
+        {
+            GetComponent<BetterJumping>().fallMultiplier = 2.5f;
         }
     }
 
@@ -70,8 +86,12 @@ public class PlayerMovement : MonoBehaviour
 
     void Jump()
     {
-        rb.AddForce(Vector3.up * jumpForce);
-        isJumpUp = true;
+        if (startJump)
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+            isJumpUp = true;
+            startJump = false;
+        }
     }
 
     // 黏牆狀態
@@ -95,13 +115,15 @@ public class PlayerMovement : MonoBehaviour
     // 蹬牆跳
     void WallJump()
     {
-        isWallJump = true;
-        rb.velocity = new Vector2(rb.velocity.x, 0);
-        StopCoroutine(DisableMovement(0));
-        StartCoroutine(DisableMovement(.1f));
-        rb.AddForce(Vector3.up * jumpForce * 1f);
-        if (coll.wallSide == 1) rb.AddForce(-Vector3.right * jumpForce * 0.8f);
-        if (coll.wallSide == -1) rb.AddForce(Vector3.right * jumpForce * 0.8f);
+        if (isWallJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+            StopCoroutine(DisableMovement(0));
+            StartCoroutine(DisableMovement(.1f));
+            rb.AddForce(Vector3.up * jumpForce * 1f);
+            if (coll.wallSide == 1) rb.AddForce(-Vector3.right * jumpForce * 0.8f);
+            if (coll.wallSide == -1) rb.AddForce(Vector3.right * jumpForce * 0.8f);
+        }
     }
 
     IEnumerator DisableMovement(float time)
