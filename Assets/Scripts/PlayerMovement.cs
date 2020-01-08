8cbaf2ccc32207d022646a9a82d9499c30d9fdc3
Halaa <neil841004@gmail.com>
2020-01-08 18:56:13 +0800

    方塊轉向

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -22,6 +22,10 @@ public class PlayerMovement : MonoBehaviour
     public bool canMove = true;
     bool speedTime;
 
+    [Space]
+    [Header("Object")]
+    public GameObject cube;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -30,16 +34,20 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-
-        if (Input.GetButtonDown("Jump")) // 跳躍狀態判斷
+        // 跳躍狀態判斷
+        if (Input.GetButtonDown("Jump")) 
         {
             if ((!IsPushWall() && coll.OnGround()) || (IsPushWall() && coll.OnGround())) startJump = true;
             else if (IsPushWall() && !coll.OnGround()) isWallJump = true;
         }
+
+        //判斷落下
         if (rb.velocity.y < 0)
         {
             isJumpUp = false;
         }
+
+        // 重置
         if (Input.GetKeyDown(KeyCode.R))
         {
             transform.position = new Vector3(0, 2.75f, -0.5f);
@@ -50,6 +58,8 @@ public class PlayerMovement : MonoBehaviour
         Move();
         Jump();
         if (IsPushWall() && !coll.OnGround()) WallJump();
+
+        // 短按跳躍落下
         if (isJumpUp)
         {
             if (Input.GetButtonUp("Jump"))
@@ -57,6 +67,8 @@ public class PlayerMovement : MonoBehaviour
                 rb.velocity = new Vector3(rb.velocity.x, 0);
             }
         }
+
+        //下墜設最大值
         if (!isJumpUp)
         {
             if (rb.velocity.y <= fallSpeedMax)
@@ -64,19 +76,30 @@ public class PlayerMovement : MonoBehaviour
                 rb.velocity = new Vector3(rb.velocity.x, fallSpeedMax);
             }
         }
+
+        // 滑牆
         if (IsPushWall() && !isWallJump)
         {
             rb.velocity = new Vector2(0, rb.velocity.y);
-            GetComponent<BetterJumping>().fallMultiplier = 0.2f;
-            if (rb.velocity.y <= fallSpeedMax * 0.5f)
+            GetComponent<BetterJumping>().fallMultiplier = 0.15f;
+            cube.transform.eulerAngles = new Vector3(0,Input.GetAxis("Horizontal")*-90,0);
+            if (rb.velocity.y <= fallSpeedMax * 0.4f)
             {
-                rb.velocity = new Vector3(rb.velocity.x, fallSpeedMax * 0.5f);
+                rb.velocity = new Vector3(rb.velocity.x, fallSpeedMax * 0.4f);
             }
         }
         if (!IsPushWall())
         {
             GetComponent<BetterJumping>().fallMultiplier = 2.5f;
+            cube.transform.eulerAngles = new Vector3(rb.velocity.y*0.7f,Input.GetAxis("Horizontal")*-60,0);
         }
+
+        // 蹬牆跳後撞牆不會卡住
+        if(isWallJump && coll.OnWall()){
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        // 蹬牆跳返回加速度
         if (speedTime)
         {
             if (speed < 8) speed += 0.9f;
@@ -131,10 +154,10 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, 0);
             StopCoroutine(DisableMovement(0));
-            StartCoroutine(DisableMovement(.1f));
+            StartCoroutine(DisableMovement(.13f));
             rb.AddForce(Vector3.up * jumpForce * 1f);
-            if (coll.wallSide == 1) rb.AddForce(-Vector3.right * jumpForce * 1f);
-            if (coll.wallSide == -1) rb.AddForce(Vector3.right * jumpForce * 1f);
+            if (coll.wallSide == 1) rb.AddForce(-Vector3.right * jumpForce * .9f);
+            if (coll.wallSide == -1) rb.AddForce(Vector3.right * jumpForce * .9f);
         }
     }
 
