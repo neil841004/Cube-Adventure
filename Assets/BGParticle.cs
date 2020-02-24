b99d3473352d3aba3548011b4e2f9f149c4fd925
Halaa <neil841004@gmail.com>
2020-02-24 20:14:00 +0800

    視覺添加

--- Assets/BGParticle.cs
+++ Assets/BGParticle.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class BGParticle : MonoBehaviour
 {
-    public GameObject player;
+    public Transform player;
+    ParticleSystem bg;
+    
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var bg = this.GetComponent<ParticleSystem>().shape;
+        bg.position = new Vector3(player.position.x,player.position.y,0);
     }
 }
